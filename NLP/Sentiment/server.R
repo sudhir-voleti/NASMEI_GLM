84e55376efe8424fdda7a58926de82fc3a5b7f95
Sudhir Voleti <sudhir_voleti@isb.edu>
2022-12-16 15:24:43 +0530

    Add files via upload

---
+++ NLP/Sentiment/server.R
@@ -0,0 +1,479 @@
+#################################################
+#         tidy sentiments                       #
+#################################################
+
+library("shiny")
+library("tidytext")
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+library("DT")
+library("reshape2")
+library("wordcloud")
+library("plotly")
+library("sentimentr")
+
+#--------------------------------------------#
+
+
+shinyServer(function(input, output,session) {
+  set.seed=2092014   
+  
+lexicon_data<-read.csv('sentiments.csv',stringsAsFactors=FALSE)# read lexcicons from app folder
+  
+  
+  output$dictionary <- renderUI({
+    if (input$lexicon != 'userdefined') return(NULL)
+    
+    fileInput("user_dict", "Upload User-Defined Dictionary")
+      })
+  
+  userdictionary = reactive({
+    if (input$lexicon != 'userdefined') return(NULL)
+    userdictionary = read.csv(input$user_dict$datapath,header=TRUE, sep = ",", stringsAsFactors = F)
+      })
+
+  dataset <- reactive({
+        if (is.null(input$file)) {return(NULL)}
+    else {
+      text  = readLines(input$file$datapath)
+      text = text[text!=""]
+      
+      if (length(text[text !=""]) == 0 ) stop (print("Null Vector :( "))
+      
+      if (length(text) == 1) {
+        textdf = data_frame(text0 = text) %>% 
+          unnest_tokens(text, text0, token = "sentences")
+      } else {
+        textdf = data_frame(text = text)  
+      }
+      
+    }
+    return(textdf)
+      })
+  
+    output$ValencesShifted <- renderDataTable({
+      datum <- sentimentr::get_sentences(dataset()$text)
+      a0 <- sentimentr::extract_sentiment_terms(datum)
+      b0 <- sentimentr::sentiment(datum)
+      a00 <- data.frame(a0, b0[,4])
+      return(a00)
+      #dataset()
+    }, options = list(aLengthMenu = c(5, 30, 50), iDisplayLength = 5, bSortClasses = TRUE,
+                    bAutoWidth = FALSE,
+                    aoColumn = list(list(sWidth = "150px", sWidth = "30px",
+                                         sWidth = "30px", sWidth = "30px"))
+    ))
+  
+    stopw = reactive({
+        # input = list(stopw = "have had samsung")
+        stopwords = data_frame(text = input$stopw) %>%
+        mutate(linenumber = row_number()) %>%
+        ungroup() %>%
+        unnest_tokens(word, text)
+        stopwords
+    })
+  
+    sent.df = reactive({
+      textdf = dataset()
+      
+      if (input$lexicon == 'userdefined'){
+          sent = textdf %>%
+          mutate(linenumber = row_number()) %>%
+          ungroup() %>%
+          unnest_tokens(word, text) %>%
+          # anti_join(stopwords, by="word") %>%
+          anti_join(stopw(), by="word") %>%
+          # inner_join(tbl_df(userdictionary)) 
+          inner_join(tbl_df(userdictionary()))
+        }    else {
+        sent = textdf %>%
+          mutate(linenumber = row_number()) %>%
+          ungroup() %>%
+          unnest_tokens(word, text) %>% 
+          anti_join(stopw(), by="word") %>% 
+          inner_join(lexicon_data%>%filter(lexicon==input$lexicon)) 
+        }
+      
+      return(sent)
+    })
+  
+
+  sentiments_cdf =  reactive ({
+    # return(tidy.sentiment(dataset(), lexicon = input$lexicon))
+    
+    if (input$lexicon %in% c('userdefined',"AFINN")){
+      sentiments_cdf = sent.df() %>%
+        # sentiments = sent %>%
+        group_by(index = linenumber %/% 1) %>% 
+        summarise(sentiment = sum(score)) %>% 
+        mutate(method = input$lexicon)
+      
+      
+    }  else {
+      sentiments_cdf = sent.df()[,-c(4,5)] %>%
+        count(sentiment, index = linenumber %/% 1, sort = TRUE) %>%
+        mutate(method = input$lexicon)
+      
+    }
+    
+    return(sentiments_cdf)
+    
+    
+  })
+
+  # output$chk = renderPrint({
+  #   sentiments_cdf()
+  # })
+  
+  
+  dat = reactive({
+    
+    dat1 = sentiments_cdf()[(sentiments_cdf()$sentiment %in% c("positive", "negative") ),]
+    dat2 = sentiments_cdf()[(sentiments_cdf()$sentiment %in% c("uncertainty","litigious","constraining","superfluous") ),]
+    dat3 = sentiments_cdf()[(sentiments_cdf()$sentiment %in% c("joy", "trust","surprise","anticipation") ),]
+    dat4 = sentiments_cdf()[(sentiments_cdf()$sentiment %in% c("anger", "disgust","fear", "sadness") ),]
+    
+    if (input$lexicon %in% c("AFINN","userdefined")) {
+      out = list(sentiments_cdf())
+    } else if (input$lexicon == "nrc") {
+      out = list(dat3,dat4,dat1)
+    } else if (input$lexicon == "bing") {
+      out = list(dat1)
+    } else if (input$lexicon == "loughran") {
+      out = list(dat2,dat1)
+    }
+    return(out)
+  })
+  
+    output$sent.plots <- renderUI({
+    if (is.null(input$file)) {return(NULL)}
+    else {
+      
+      if (input$lexicon %in% c("AFINN","userdefined")) {
+        k = 1
+      } else if (input$lexicon == "nrc") {
+        k = 3
+      } else if (input$lexicon == "bing") {
+        k = 1
+      } else if (input$lexicon == "loughran") {
+        k = 2
+      }
+        plot_output_list <- lapply(1:k, function(i) {
+        plotname <- paste("plot", i, sep="")
+        plotlyOutput(plotname, height = 700, width = 700)
+        
+        # plotOutput(plotname, height = 700, width = 700)
+      })
+      # Convert the list to a tagList - this is necessary for the list of items
+      # to display properly.
+      do.call(tagList, plot_output_list)
+    }
+  })
+  
+  
+  max_plots = 3
+  
+  for (i in 1:max_plots) {
+    # Need local so that each item gets its own number. Without it, the value
+    # of i in the renderPlot() will be the same across all instances, because
+    # of when the expression is evaluated.
+    local({
+      
+      my_i <- i 
+      plotname <- paste("plot", my_i, sep="")
+      
+        output[[plotname]] <- renderPlotly({
+          if (input$lexicon %in% c("AFINN",'userdefined')) {
+            plot_ly(dat()[[my_i]],x = ~index, y = ~ sentiment,type = "bar")
+            # plot_ly(sentiments,x = ~index, y = ~ sentiment,type = "bar")
+            #      aes(index, sentiment)) +     # index is x col, n is y col. fill=?
+            # geom_bar(alpha = 1, stat = "identity", position = "identity", show.legend = FALSE)      # stat=?
+          } else {
+            
+          ggplot(dat()[[my_i]], 
+            aes(index, n, fill = sentiment)) +     # index is x col, n is y col. fill=?
+            geom_bar(alpha = 0.8, stat = "identity", show.legend = FALSE) +     # stat=?
+            facet_wrap(~sentiment, ncol = 2, scales = "free_x")     # so cool.    
+          }
+        })
+      })
+  }
+  
+  
+  output$event <- renderPrint({
+    d <- event_data("plotly_hover")
+    if (is.null(d)) "Hover on a point!" else d
+  })
+  
+  
+  output$word.cloud <- renderPlot({
+    
+    if (is.null(input$file)) {return(NULL)}
+    else {
+    
+    if (input$lexicon  %in% c("nrc","bing","loughran")) {
+      sent.df() %>%
+      count(word, sentiment, sort = TRUE) %>%
+      acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+       comparison.cloud( #colors = c("#F8766D", "#00BFC4"),
+                       max.words = 300)
+    } else {
+      
+        sent.df() %>%
+        count(word, score, sort = TRUE) %>%
+        acast(word ~ score, value.var = "n", fill = 0) %>%
+        comparison.cloud( #colors = c("#F8766D", "#00BFC4"),
+        max.words = 100)
+    }
+    }
+  })
+  
+
+  
+  
+  
+  output$count <- renderDataTable({
+    
+    if (is.null(input$file)) {return(NULL)}
+    else {
+      
+    # textdf = dataset()
+    
+    if (input$lexicon %in% c("nrc","bing","loughran")) {
+        wc = sent.df() %>%
+        count(word, sentiment, sort = TRUE) %>%
+        acast(word ~ sentiment, value.var = "n", fill = 0) 
+        #   %>% comparison.cloud( #colors = c("#F8766D", "#00BFC4"),
+        #   max.words = 300)
+      
+    } else {
+      
+        wc = sent.df() %>%
+        count(word, score, sort = TRUE) %>%
+        acast(word ~ score, value.var = "n", fill = 0) 
+        # %>% comparison.cloud( #colors = c("#F8766D", "#00BFC4"),
+        #   max.words = 100)
+    }
+    
+    wc1 = data.frame(wc)
+
+    if (input$lexicon %in% c("AFINN","userdefined")){
+     neg =grep("\\.",colnames(wc1))
+     vec = c(rep("neg_",length(neg)), rep("pos_",(length(colnames(wc1))-length(neg))))
+     cnames = paste(vec,gsub("X|X\\.","",colnames(wc1)))
+     colnames(wc1) = cnames
+    }
+    wc1 = wc1[order(wc1[,1], decreasing = T),]
+    return(wc1)
+    }
+  })
+  
+  #----------------------------------------------------#
+  
+  sentiments.index =  reactive ({
+    # return(tidy.sentiment(dataset(), lexicon = input$lexicon))
+    textdf = dataset()[input$index,] %>% unnest_tokens(text, text, token = "sentences")
+  
+    if (input$lexicon == "nrc") {
+        sent = textdf %>%
+        mutate(linenumber = row_number()) %>%
+        ungroup() %>%
+        unnest_tokens(word, text) %>%
+        anti_join(stopw(), by="word") %>%
+        inner_join(lexicon_data%>%filter(lexicon=='nrc')) %>% #----changes required--#
+        count(sentiment, Sentence.No = linenumber %/% 1, sort = TRUE) %>%
+        mutate(method = "nrc")
+    }
+    
+    if (input$lexicon == "bing") {
+      sent = textdf %>%
+        mutate(linenumber = row_number()) %>%
+        ungroup() %>%
+        unnest_tokens(word, text) %>%
+        anti_join(stopw(), by="word") %>%
+        inner_join(lexicon_data%>%filter(lexicon=='bing')) %>% #----changes required--#
+        count(sentiment, Sentence.No = linenumber %/% 1, sort = TRUE) %>%
+        mutate(method = "bing")
+    }
+    
+    if (input$lexicon == "AFINN") {
+      sent = textdf %>%
+        mutate(linenumber = row_number()) %>%
+        ungroup() %>%
+        unnest_tokens(word, text) %>%
+        anti_join(stopw(), by="word") %>%
+        inner_join(lexicon_data%>%filter(lexicon=='AFINN')) %>% #----changes required--#
+        group_by(Sentence.No = linenumber %/% 1) %>% 
+        summarise(sentiment = sum(score)) %>% 
+        mutate(method = "afinn")
+    }
+    
+    if (input$lexicon == "loughran") {
+      sent = textdf %>%
+        mutate(linenumber = row_number()) %>%
+        ungroup() %>%
+        unnest_tokens(word, text) %>%
+        anti_join(stopw(), by="word") %>%
+        inner_join(lexicon_data%>%filter(lexicon=='loughran')) %>% #----changes required--#
+        count(sentiment, Sentence.No = linenumber %/% 1, sort = TRUE) %>%
+        mutate(method = "loughran")
+    }
+    
+    if (input$lexicon == "userdefined") {
+        sent = textdf %>%
+        mutate(linenumber = row_number()) %>%
+        ungroup() %>%
+        unnest_tokens(word, text) %>%
+        anti_join(stopw(), by="word") %>%
+        inner_join(tbl_df(userdictionary())) %>%
+        group_by(Sentence.No = linenumber %/% 1) %>% 
+        summarise(sentiment = sum(score)) %>% 
+        mutate(method = "userdefined")
+    }
+    
+    return(sent)
+    
+  })
+  
+
+  # output$sent.plot.index <- renderPlot ({
+  # 
+  #   if (input$lexicon != "afinn") {
+  #     ggplot(sentiments.index(),
+  #            aes(index, n, fill = sentiment)) +     # index is x col, n is y col. fill=?
+  #       geom_bar(alpha = 0.8, stat = "identity", show.legend = FALSE) +     # stat=?
+  #       facet_wrap(~sentiment, ncol = 2, scales = "free_x")     # so cool.
+  #   }   else  {
+  #     ggplot(sentiments.index(),
+  #            aes(index, sentiment)) +     # index is x col, n is y col. fill=?
+  #       geom_bar(alpha = 1, stat = "identity", position = "identity", show.legend = FALSE)      # stat=?
+  #   }
+  # 
+  # })
+  
+  t1 = reactive({
+    if (is.null(input$file)) {return(NULL)}
+    else {
+      
+      textdf = dataset()
+      
+      if (input$lexicon == 'userdefined'){
+          worddf = textdf %>%
+          mutate(linenumber = row_number()) %>%
+          ungroup() %>%
+          unnest_tokens(word, text) %>%
+          # anti_join(stopwords, by="word") %>%
+          anti_join(stopw(), by="word") %>%
+          # inner_join(tbl_df(userdictionary)) 
+          inner_join(tbl_df(userdictionary())) %>% 
+          unique()
+      }    else {
+          worddf = textdf %>%
+          mutate(linenumber = row_number()) %>%
+          ungroup() %>%
+          unnest_tokens(word, text) %>% 
+          anti_join(stopw(), by="word") %>% 
+          inner_join(lexicon_data%>%filter(lexicon==input$lexicon)) %>% 
+          unique()
+      }
+      
+      worddf = data.frame(worddf)
+      
+      if (input$lexicon %in%  c("nrc","bing","loughran")) {
+        wdf = data.frame(NULL)
+        for (i in unique(worddf$linenumber)) {
+          tempd = worddf[worddf$linenumber == i,]
+          se = unique(tempd$sentiment)
+          se = se[order(se)]
+          for (s in se){
+            t = paste(tempd[tempd$sentiment == s,'word'],collapse = ", ")
+            dft = data.frame(index = i, sentiment = s, words = t)
+            wdf = rbind(wdf, dft)
+          }
+        } 
+      }   else {
+        wdf = data.frame(NULL)
+        for (i in unique(worddf$linenumber)) {
+          tempd = worddf[worddf$linenumber == i,]
+          se = unique(tempd$score)
+          se = se[order(se)]
+          for (s in se){
+            t = paste(tempd[tempd$score == s,'word'],collapse = ", ")
+            dft = data.frame(index = i, sentiment = s, words = t)
+            wdf = rbind(wdf, dft)
+          }
+        }
+        
+      }
+      
+      wdf1 = wdf[wdf$index == input$index,]
+      # test = merge(tb,wdf ,by.x ="index", by.y= "index", all.y=T)
+      return(wdf1)
+    }
+    
+  })
+  
+  output$table <- renderDataTable({
+    datatable(t1(), rownames = F)#
+  }, options = list(lengthMenu = c(5, 30, 50), pageLength = 30))
+  
+  
+  
+  t2 = reactive({
+    if (is.null(input$file)) {return(NULL)}
+    else {
+      
+      tb = sentiments.index()
+      tx = dataset()[input$index,] %>% unnest_tokens(text, text, token = "sentences")
+      
+      y1 = data.frame(tx, Sentence.No= 1:nrow(tx))
+      
+      test = merge(tb,y1 ,by.x ="Sentence.No", by.y= "Sentence.No", all.y=T)
+      return(test)
+    }
+    
+  })
+  
+  output$table2 <- renderDataTable({
+    datatable(t2(), rownames = F)
+  }, options = list(lengthMenu = c(5, 30, 50), pageLength = 30))
+  
+  output$Scores_by_Lexicon <- renderDataTable({
+    return(sentiments_cdf())
+  })
+
+  #----------------------------------------------------#
+  
+  output$downloadData1 <- downloadHandler(
+    filename = function() { "Nokia_Lumia_reviews.txt" },
+    content = function(file) {
+      writeLines(readLines("data/Nokia_Lumia_reviews.txt"), file)
+    }
+  )
+  
+  output$downloadData2 <- downloadHandler(
+    filename = function() { "Sentiments Scores.csv" },
+    content = function(file) {
+      write.csv(sentiments_cdf(), file, row.names=F)
+    })
+  
+  output$downloadData3 <- downloadHandler(
+    filename = function() { "User Defined Dictionary.csv" },
+    content = function(file) {
+      write.csv(read.csv("data/user defined dictionary.csv",stringsAsFactors = F), file, row.names=F)
+    })
+  
+  output$downloadData4 <- downloadHandler(
+    filename = function() { "OnePlus.txt" },
+    content = function(file) {
+       writeLines(readLines("data/onePlus8T_reviews.txt.txt "), file)
+    })
+  
+  output$downloadData5 <- downloadHandler(
+    filename = function() { "Bahubali_reviews.txt" },
+    content = function(file) {
+       writeLines(readLines("data/Bahubali_reviews.txt "), file)
+    })  
+
+})
---
+++ NLP/Sentiment/ui.R
@@ -0,0 +1,115 @@
+#################################################
+#           tidy sentiment Analysis             #
+#################################################
+
+library("shiny")
+library("tidytext")
+library("tidyr")
+library("dplyr")
+library("ggplot2")
+library("DT")
+library("reshape2")
+library("wordcloud")
+library("plotly")
+
+#--------------------------------------------#
+
+shinyUI(fluidPage(
+  
+   # tags$head(includeScript("google_analytics.js")),
+  
+  #titlePanel("Sentiment Analysis with tidytext"),
+  titlePanel(title=div(img(src="logo.png",align='right'),"Sentiment Analysis with tidytext")),
+  
+  # Input in sidepanel:
+  sidebarPanel(
+    fileInput("file", "Upload text file"),  
+    
+    selectInput("lexicon", "Sentiment Dictionary",
+    c("AFINN","bing","nrc","loughran","User Defined"="userdefined"), selected = "AFINN"),
+    
+    uiOutput("dictionary"),
+    textInput("stopw", ("Enter stop words separated by comma(,)"), value = "will,can"),
+    
+    numericInput("index", "Document Index", 1)
+    # submitButton(text = "Apply Changes", icon("refresh"))
+    
+  ),
+  
+  # Main Panel:
+  mainPanel( 
+    
+    
+    tabsetPanel(type = "tabs",
+                #
+                tabPanel("Overview",h4(p("How to use this App")),
+                         
+                         p("To use this app you need a document corpus in txt file format. Make sure each document is separated from another document with a new line character.
+                           To do basic sentiment analysis in your text corpus, click on Browse in left-sidebar panel and upload the txt file. Once the file is uploaded it will do the computations in 
+                            back-end with default inputs and accordingly results will be displayed in various tabs.", align = "justify"),
+	                       a(href="https://www.youtube.com/watch?v=4b7ZidC6ZOA","Youtube Link for App Navigation"),
+                         
+                         p("You can change the sentiment dictionary in left-sidebar panel. This app supports four inbuilt sentiment dictionaries and one user defined dictionary. If a user selects User Defined dictionary, then a browse file input will appear below sentiment dictionary drop-down in left-side-bar panel and user can upload the user defined dictionary. This user defined dictionary should be in csv format and first column of the dictionary should be word and second column should be score. You can download the sample user defined dictionary below.", align = "justify"),
+                         a(href="http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010","1- Afinn"),
+                         p("AFINN is a list of English words rated for valence with an integer between minus five (negative) and plus five (positive). The words have been manually labeled by Finn Arup Nielsen in 2009-2011."),
+                      
+                         a(href="https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html","2- Bing"),
+                         p("This sentiment dictionary is created by Bing Liu and collaborators. In this dictionary, words are classified as positive or negative."),
+                      
+                         a(href="http://saifmohammad.com/WebPages/NRC-Emotion-Lexicon.htm","3- NRC"),
+                         p("The NRC Emotion Lexicon is a list of English words and their associations with eight basic emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust) and two sentiments (negative and positive). The annotations were manually done by crowdsourcing."),
+                      
+                         a(href="http://www3.nd.edu/~mcdonald/Word_Lists.html","4- Loughran"),
+                         p("This dictionary is created by Tim Loughran and Bill McDonald. In this dictionary each word is classified in financial context (uncertainty, litigious, constraining, superfluous, positive, negative)"),
+                        
+                         h4(p("Download Sample user defined dictionary file")),
+                         downloadButton('downloadData3', 'Download User Defined Dictionary (works only in browser)'),br(),br(),
+                         
+                         p("In the left-side bar panel you can change the document index number and accordingly document level analysis will be updated in \"Document level Analysis\" tab", align = "justify"),
+                         
+                         p("If plots are not working in \"Sentiments - Plot\" tab then please install latest version of ggplot2. You can install ggplot2 by command - install.packages(\"ggplot2\")", align = "justify"),
+                         h4(p("Download Sample text file")),
+                         downloadButton('downloadData1', 'Download Nokia Lumia reviews txt file (works only in browser)'),br(),br(),
+                         downloadButton('downloadData4', 'Download One Plus reviews txt file (works only in browser)'),br(),br(),
+			 downloadButton('downloadData5', 'Download Bahubali reviews txt file (works only in browser)'),br(),br(),
+                         p("Please note that download will not work with RStudio interface. Download will work only in web-browsers. So open this app in a web-browser and then download the example file. For opening this app in web-browser click on \"Open in Browser\" as shown below -"),
+                         img(src = "example1.png")
+                       
+                         ),
+                
+                tabPanel("Sentiments - Plot",h4(),
+                         # verbatimTextOutput('chk'),
+                         # 
+                         uiOutput("sent.plots"),
+                         verbatimTextOutput("event")
+                         # h4("Weights Distribution of Wordcloud"),
+                         # verbatimTextOutput("dtmsummary1")
+                         ),
+                tabPanel("Sentiments - Stat",br(),
+                         plotOutput("word.cloud",height = 700, width = 700),
+                         br(),
+                         dataTableOutput("count"),
+                         br()
+                         ),
+
+                tabPanel("Document level Analysis",br(),
+                         downloadButton('downloadData2', 'Download Sentiment Scores (Works only in browser)'), br(),br(),
+                         h4("List of the most frequent sentiment words at each level"),
+                         dataTableOutput("table"),
+                         br(),
+                         h4("Document parsed into sentences and sentiment score for each sentence"),
+                         # downloadButton('downloadData4', 'Downlaod Sentiemnt Scores (Works only in browser)'), br(),br(),
+                         dataTableOutput("table2")),
+			          tabPanel("Valence Shifters", br(),
+			                   dataTableOutput("ValencesShifted")
+			            
+			          ),
+			          tabPanel("Sentiment Scores", br(),
+			                   dataTableOutput("Scores_by_Lexicon")
+			                   )
+                #                         
+                         )
+                )
+  
+)
+)
